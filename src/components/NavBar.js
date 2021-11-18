2c4db3808816cdcf5eeef8a2e6345b6a48b9d121
Bedegege <94636408+Bedegege@users.noreply.github.com>
2021-11-19 00:46:55 +0700

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,105 @@
+import {
+  Button,
+  Grid,
+  Container,
+} from "@material-ui/core";
+import Link from "next/link";
+import NavButton from "./NavButton";
+import { Fragment } from "react";
+
+const navButtons = [
+  {
+    label: "Home",
+    path: "/"
+  },
+  {
+    label: "About Us"
+  },
+  {
+    label: "FAQ"
+  },
+  {
+    label: "News"
+  },
+  {
+    label: "Gallery"
+  }
+];
+
+const NavBar = () => {
+  return (
+    <div className="container">
+    <Grid container justifyContent="center">
+      <Container maxWidth="lg">
+        <Grid
+          container
+          style={{ paddingLeft: 32, paddingRight: 32 }}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item style={{ flexDirection: "column" }} xs={4}>
+            <Link href="/">
+              <a>
+                <img
+                  style={{ paddingTop: 10, paddingBottom: 14, width: 55 }}
+                  src="/art.svg"
+                  alt=""
+                />
+              </a>
+            </Link>
+          </Grid>
+
+          <Grid item justifyContent="flex-end" container spacing={3} xs>
+            {navButtons.map((button, i) => (
+              <Fragment key={i}>
+                  <Grid item key={i}>
+                    <NavButton
+                      key={button.path}
+                      path={button.path || ""}
+                      label={button.label}
+                    />
+                  </Grid>
+              </Fragment>
+            ))}
+          </Grid>
+
+          <Grid style={{ marginLeft: 60 }}>
+            <NavButton path="#" label="Login" />
+          </Grid>
+
+          <Grid>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{
+                background: "#8854D0",
+                borderRadius: 4,
+                textTransform: "none",
+                marginLeft: 20
+              }}
+              disableRipple
+            >
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </Grid>
+
+    <style jsx>{`
+        .container {
+          width: 100%;
+          height: 88px;
+          background-color: #ffffff;
+          border: 1px solid #f0f2f5;
+          position: fixed;
+          display: flex;
+          z-index: 99;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default NavBar
